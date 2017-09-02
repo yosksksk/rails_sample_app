9e5e58603d4b81a621308319388671006ca364d5
Yoshiki Nishioka <yoshiki.nishioka.kobe@gmail.com>
2017-09-03 00:16:39 +0900

    add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
       @user = User.find(params[:id])
+      redirect_to root_url and return unless @user.activated?
   end
 
   def new
@@ -12,16 +13,15 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-     # 保存の成功をここで扱う。
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
      render 'new'
     end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -55,7 +55,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "authenticated? should return false for a user with nil digest" do
-   assert_not @user.authenticated?('')
+    assert_not @user.authenticated?(:remember, '')
  end
 
 end
